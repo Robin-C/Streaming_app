fac45e754ca7ee62be1788ad5a219b9bcf9747e2
Robin C <r.chesne@gmail.com>
2021-10-24 14:04:35 +0200

    setting everything up

--- twitter_api_app_on_startup.sh
+++ twitter_api_app_on_startup.sh
@@ -1,9 +1,11 @@
 # install the packages to run the python scripts
+
 cd /streaming_app/twitter_api_app 
 virtualenv env_twitter_api_app
 . env_twitter_api_app/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 deactivate
+
 # keep container running
 tail -f /dev/null
